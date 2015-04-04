46bf074fe798172f0e77deb956d766608e500941
Shinkei <shinkeygentatsu@yahoo.com.mx>
2015-04-04 01:24:50 -0500

    Creation of the demo project ProductAPI

---
+++ ProductAPI/app.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/proyectox/v1/productos', methods=['GET'])
+def index():
+    return jsonify({"return": "hello world"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
